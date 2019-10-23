6f784ca670fbb2e771927b485995fe0390e4c66f
AshishAsokan <raashish020@gmail.com>
2019-10-23 21:35:02 +0530

    Navigation bar
    Navigation bar completed. Problem with send feedback button. Textarea height changes when button is highlighted

---
+++ Contact Us/navbar_js.js
@@ -0,0 +1,20 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+$(function(){
+  var str = '#len'; //increment by 1 up to 1-nelemnts
+  $(document).ready(function(){
+    var i, stop;
+    i = 1;
+    stop = 4; //num elements
+    setInterval(function(){
+      if (i > stop){
+        return;
+      }
+      $('#len'+(i++)).toggleClass('bounce');
+    }, 500)
+  });
+});
